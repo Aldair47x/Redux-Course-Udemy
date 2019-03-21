5e72f02d0e1d118a6d508f9c5d9439321884f315
Aldair Bernal <aldair47x@gmail.com>
2019-03-21 08:42:29 -0500

    Example of Store, reducers and actions comunication .

--- redux-app/src/app/contador/contador.reducer.ts
+++ redux-app/src/app/contador/contador.reducer.ts
@@ -4,7 +4,7 @@ import * as fromContador from './contador.action';
 
 
 
-export function contadorReducer( state : number = 10, action: actions) {
+export function contadorReducer( state : number = 10, action: fromContador.actions) {
     switch ( action.type ) {
         case fromContador.INCREMENTAR:
             return state + 1;
